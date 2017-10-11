fa45d201940294dd0cb9f3781a14f86133e768d4
Kevin Tate <ktate@localhost.localdomain>
2017-10-11 18:40:56 -0400

    adding headers logging

--- helloworld-html5/src/main/java/org/jboss/as/quickstarts/html5rest/HelloWorld.java
+++ helloworld-html5/src/main/java/org/jboss/as/quickstarts/html5rest/HelloWorld.java
@@ -21,6 +21,8 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 
 /**
  * A simple REST service which is able to say hello to someone using HelloService Please take a look at the web.xml where JAX-RS
@@ -37,8 +39,9 @@ public class HelloWorld {
     @POST
     @Path("/json/{name}")
     @Produces("application/json")
-    public String getHelloWorldJSON(@PathParam("name") String name) {
+    public String getHelloWorldJSON(@PathParam("name") String name, @Context HttpHeaders headers) {
         System.out.println("name: " + name);
+        System.out.println("headers: " + headers);
         return "{\"result\":\"" + helloService.createHelloMessage(name) + "\"}";
     }
 
